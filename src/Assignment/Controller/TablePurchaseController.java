05cacd5ec12ca07dda2b9be77c6179e12624109c
TranChiHuynh200065 <huynhtcgcc200065@fpt.edu.vn>
2022-09-13 09:04:17 +0700

    Update hello

--- src/Assignment/Controller/TablePurchaseController.java
+++ src/Assignment/Controller/TablePurchaseController.java
@@ -339,7 +339,7 @@ public class TablePurchaseController {
             motorcycleShopManagementSystem.JDatePickerPurchase.getJFormattedTextField().setValue(c);
         }catch (Exception e){
             //Do nothing
-            System.out.println(e);
+            System.out.println("Hello con heo");
         }
     }
 
